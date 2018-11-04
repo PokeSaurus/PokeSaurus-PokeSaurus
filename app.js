2bb6931d2def5b8eb0d2b015c2db00f3d29bf8f8
PokeSaurus <44736579+PokeSaurus@users.noreply.github.com>
2018-11-04 00:39:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ search.addWidget(
   })
 );
 
+
 search.addWidget(
   instantsearch.widgets.clearAll({
     container: '#clear-all',
@@ -36,48 +37,6 @@ search.addWidget(
     },
     autoHideContainer: false,
     clearsQuery: true,
-  })
-);
-
-search.addWidget(
-    instantsearch.widgets.refinementList({
-      container: '#type1||type2-refinement',
-      attributeName: 'type',
-      templates: {
-        header: 'Type'
-      },
-      searchForFacetValues: {
-        placeholder: 'Search by types',
-        templates: {
-          noResults: '<div class="sffv_no-results">No matching types.</div>'
-        }
-      }
-    })
-  );
-
-
-search.addWidget(
-  instantsearch.widgets.sortBySelector({
-    container: '#sort-by-container',
-    indices: [
-      {name: 'instant_search', label: 'Most relevant'},
-      {name: 'instant_search_ID_asc', label: 'Ascending ID#'},
-      {name: 'instant_search_ID_desc', label: 'Descending ID#'},
-      {name: 'instant_search_HP_asc', label: 'Ascending HP'},
-      {name: 'instant_search_HP_desc', label: 'Descending HP'},
-      {name: 'instant_search_attack_asc', label: 'Ascending attack'},
-      {name: 'instant_search_attack_desc', label: 'Descending attack'},
-      {name: 'instant_search_defence_asc', label: 'Ascending defence'},
-      {name: 'instant_search_defence_desc', label: 'Descending defence'},
-      {name: 'instant_search_spAtk_asc', label: 'Ascending spAtk'},
-      {name: 'instant_search_spAtk_desc', label: 'Descending spAtk'},
-      {name: 'instant_search_spDef_asc', label: 'Ascending spDef'},
-      {name: 'instant_search_spDef_desc', label: 'Descending spDef'},
-      {name: 'instant_search_speed_asc', label: 'Ascending speed'},
-      {name: 'instant_search_speed_desc', label: 'Descending speed'},
-      {name: 'instant_search_totalBaseStats_asc', label: 'Ascending Total Base Stats'},
-      {name: 'instant_search_totalBaseStats_desc', label: 'Descending Total Base Stats'},
-    ]
   })
 );
 
